06e1a896f5d70e6d5bbe3edab2d481ad1a0b3e0c
jack <dl@google.com>
2022-01-17 15:49:54 +0800

    update

--- acExt/action.go
+++ acExt/action.go
@@ -1,9 +1,16 @@
 package acExt
 
 import (
-	"github.com/chromedp/cdproto/cdp"
-	"github.com/chromedp/chromedp"
 	"context"
+	"fmt"
+	"github.com/chromedp/cdproto/cdp"
+	"github.com/chromedp/cdproto/dom"
+	"github.com/chromedp/cdproto/page"
+	"github.com/chromedp/cdproto/runtime"
+	"github.com/chromedp/chromedp"
+	"math"
+	"strings"
+	"time"
 )
 type StdLogger interface {
 	Print(...interface{})
@@ -122,4 +129,80 @@ func Print(args ...interface{}) chromedp.QueryAction {
 		}
 		return nil
 	})
+}
+func WaitForJS(expression string, res interface{}, opts ...chromedp.PollOption) chromedp.ActionFunc {
+
+	return func(ctx context.Context) error {
+		tm:=time.NewTicker(time.Second)
+		defer tm.Stop()
+		for {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case <-tm.C:
+				err:=chromedp.Poll(expression,res,opts...).Do(ctx)
+				if err!=nil{
+					if strings.Contains(err.Error(),"Cannot find context with specified id (-32000)")==false&&strings.Contains(err.Error(),"Execution context was destroyed. (-32000)"){
+						return err
+					}
+				}else{
+					if res!=nil{
+						return nil
+					}
+				}
+			}
+		}
+		return nil
+	}
+}
+func GetClientSize(sel string, viewPort *page.Viewport, opts ...chromedp.QueryOption)chromedp.QueryAction{
+	getClientRectJS:= `function getClientRect() {
+		const e = this.getBoundingClientRect(),
+		t = this.ownerDocument.documentElement.getBoundingClientRect();
+		return {
+			x: e.left - t.left,
+			y: e.top - t.top,
+			width: e.width,
+			height: e.height,
+		};
+	}`
+	return chromedp.QueryAfter(sel, func(ctx context.Context, id runtime.ExecutionContextID, nodes ...*cdp.Node) error {
+		if len(nodes) < 1 {
+			return fmt.Errorf("selector %q did not return any nodes", sel)
+		}
+		if err := CallFunctionOnNode(ctx, nodes[0], getClientRectJS, viewPort); err != nil {
+			return err
+		}
+		x, y := math.Round(viewPort.X), math.Round(viewPort.Y)
+		viewPort.Width, viewPort.Height = math.Round(viewPort.Width+viewPort.X-x), math.Round(viewPort.Height+viewPort.Y-y)
+		viewPort.X, viewPort.Y = x, y
+		viewPort.Scale=1
+
+		return nil
+
+	},append(opts, chromedp.NodeVisible)...)
+}
+
+func CallFunctionOnNode(ctx context.Context, node *cdp.Node, function string, res interface{}, args ...interface{}) error {
+	r, err := dom.ResolveNode().WithNodeID(node.NodeID).Do(ctx)
+	if err != nil {
+		return err
+	}
+	err = chromedp.CallFunctionOn(function, &res,
+		func(p *runtime.CallFunctionOnParams) *runtime.CallFunctionOnParams {
+			return p.WithObjectID(r.ObjectID)
+		},
+		args...,
+	).Do(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	// Try to release the remote object.
+	// It will fail if the page is navigated or closed,
+	// and it's okay to ignore the error in this case.
+	_ = runtime.ReleaseObject(r.ObjectID).Do(ctx)
+
+	return nil
 }
